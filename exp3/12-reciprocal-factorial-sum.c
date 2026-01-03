e8b3db38f7c763351f7681c4bca15289428dcaa0
SharmaRahul111 <sharmarahul111.psr@gmail.com>
2026-01-03 15:00:16 +0545

    exp3: reciprocal sum Qn: 12,15

---
+++ exp3/12-reciprocal-factorial-sum.c
@@ -0,0 +1,17 @@
+// WAP to prinf=t sum of series
+// 1/1!+1/2!+1/3!+1/4!+...+1/n!
+#include<stdio.h>
+int main(){
+  int n,i,j,factorial;
+  float sum=0;
+  printf("Enter n:");
+  scanf("%d", &n);
+  for(i=1;i<=n;i++){
+    factorial = 1;
+    for (j=1;j<=i;j++) {
+      factorial *= j;
+    }
+    sum += 1.0/factorial;
+  }
+  printf("The sum is: %f\n", sum);
+}
---
+++ exp3/15-reciprocal-sum.c
@@ -0,0 +1,13 @@
+// WAP to prinf=t sum of series
+// 1/1+1/2+1/3+1/4+...+1/n
+#include<stdio.h>
+int main(){
+  int n,i;
+  float sum=0;
+  printf("Enter n:");
+  scanf("%d", &n);
+  for(i=1;i<=n;i++){
+    sum += 1.0/i;
+  }
+  printf("The sum is: %f\n", sum);
+}
